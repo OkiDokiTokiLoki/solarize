dcebf6c3b3182ae017bd65c715a84712b15af960
tokiloki <codebadger1@gmail.com>
2021-11-09 18:50:38 +0200

    dark/light themes created

--- solarize.js
+++ solarize.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const darkButton = document.querySelector('#dark');
+const lighthButton = document.querySelector('#light');
+const solarButton = document.querySelector('#solar');
+const body = document.body;
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+}
+
+lighthButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+}
+
