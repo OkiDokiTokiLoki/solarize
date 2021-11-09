7ee251ec1590d149ec484355b72a02dca957f34c
tokiloki <codebadger1@gmail.com>
2021-11-09 18:57:03 +0200

    solar theme functionality created

--- solarize.js
+++ solarize.js
@@ -5,9 +5,25 @@ const body = document.body;
 
 darkButton.onclick = () => {
     body.classList.replace('light', 'dark');
-}
+};
 
 lighthButton.onclick = () => {
     body.classList.replace('dark', 'light');
-}
+};
 
+solarButton.onclick = () => {
+    if(body.classList.contains('solar')){
+        
+        body.classList.remove('solar');
+        
+        solarButton.style.cssText = 
+            `--bg-solar: var(--yellow);`
+        solarButton.innerText = 'solarize';
+
+    } else {
+        solarButton.style.cssText = 
+            `--bg-solar: white;`
+        body.classList.add('solar');
+        solarButton.innerText = 'normalize';
+    }
+};
